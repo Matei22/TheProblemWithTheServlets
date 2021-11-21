821ff716311d3fa4fb1756c17b0f806ad43bb2a6
Matei22 <60110280+Matei22@users.noreply.github.com>
2021-11-21 21:51:53 +0200

    Add files via upload

---
+++ MyFirstWebApp/src/main/java/com/MyServlet.java
@@ -0,0 +1,55 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class Servlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<h1> Hello World, Service Method</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		String name=request.getParameter("fname");
+		out.print("Hello World, doGet Method");
+		out.println("<br/> Hello " + name);
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		String name=request.getParameter("fname");
+		out.print("Hello World, doPost Method");
+		out.println("<br/> Hello " + name);
+}
+
+}
